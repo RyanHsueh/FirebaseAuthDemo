533321c70182c1ee9eae47d5d2c4f698a535b693
Ryan Hsueh <freestyle0103@gmail.com>
2018-08-22 15:40:27 +0800

    email verification

--- app/src/main/java/com/example/ryanhsueh/firebaseauthdemo/MainActivity.java
+++ app/src/main/java/com/example/ryanhsueh/firebaseauthdemo/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.ryanhsueh.firebaseauthdemo;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -20,12 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        FirebaseUser user = mAuth.getCurrentUser();
-        if (user != null) {
-            TextView textEmail = findViewById(R.id.text_email);
-            textEmail.setText(user.getEmail());
-        }
-
     }
 
     public void onSignOut(View view) {
@@ -34,5 +33,45 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    public void getUserProfile(View view) {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("display name:");
+            sb.append(user.getDisplayName());
+            sb.append("\n");
+            sb.append("email:");
+            sb.append(user.getEmail());
+            sb.append("\n");
+            sb.append("photo url:");
+            sb.append(user.getPhotoUrl());
+            sb.append("\n");
+            sb.append("is email verified:");
+            sb.append(user.isEmailVerified());
+            sb.append("\n");
+            sb.append("uid:");
+            sb.append(user.getUid());
+
+            TextView textUserInfo = findViewById(R.id.text_user_info);
+            textUserInfo.setText(sb.toString());
+        }
+    }
+
+    public void sendVerifyEmail(View view) {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "驗證Email已寄出", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "驗證Email寄送失敗", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
