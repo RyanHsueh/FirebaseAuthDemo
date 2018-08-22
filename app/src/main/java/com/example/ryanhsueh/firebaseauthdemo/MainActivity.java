e3327cb57dc5bfda2968d2c942387ea314df2685
Ryan Hsueh <freestyle0103@gmail.com>
2018-08-22 16:31:57 +0800

    Added function for re-authenticate

--- app/src/main/java/com/example/ryanhsueh/firebaseauthdemo/MainActivity.java
+++ app/src/main/java/com/example/ryanhsueh/firebaseauthdemo/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.ryanhsueh.firebaseauthdemo;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -94,6 +100,49 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
+        }
+    }
+
+    /*
+        官網強調當你刪除帳號、設定主要 email 以及改變密碼的時候
+        都必須做 Re-authenticate 否則有機會會丟出 FirebaseAuthRecentLoginRequiredException
+        因此我們在呼叫 FirebaseUser 物件的 updateEmail
+        要寫在 FirebaseUser 物件的 reauthenticate callback 完成以後才可以改變 Email
+     */
+    public void reauthenticate(View view) {
+        final FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            final View layout = LayoutInflater.from(this).inflate(R.layout.dlg_user_account, null);
+            new AlertDialog.Builder(this)
+                    .setView(layout)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            TextInputLayout tl_username = layout.findViewById(R.id.tl_username);
+                            TextInputLayout tl_password = layout.findViewById(R.id.tl_password);
+
+                            String email = tl_username.getEditText().getText().toString();
+                            String password = tl_password.getEditText().getText().toString();
+
+                            AuthCredential credential = EmailAuthProvider.getCredential(email, password);
+                            user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(MainActivity.this, "重新認證成功", Toast.LENGTH_SHORT).show();
+
+                                        // 重新認證成功後，才可修改用戶Email or Password
+                                        
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "重新認證失敗", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
+                            dialog.dismiss();
+                        }
+                    })
+                    .show();
         }
     }
 }
